e7625522aecb4c07869b8c6fa812def8477106d0
Adam Watkins <adam@stupidpupil.co.uk>
2021-04-27 18:48:00 +0100

    Add prefix test for org

--- tests/testthat/test-add_fields_based_on_org_code.R
+++ tests/testthat/test-add_fields_based_on_org_code.R
@@ -10,3 +10,17 @@ test_that("7A4 will produce Abbr, Name and FullName as expected", {
   expect_equal(length(names(example)), 4)
 
 })
+
+
+test_that("7A4 will produce Abbr, Name and FullName as expected with prefix", {
+
+  example <- tibble(
+    AdmissionSiteOrgCode = c("7A4")
+  ) %>% add_fields_based_on_org_code(prefix='AdmissionSite', fields = c('OrgAbbr', 'OrgName', 'OrgFullName'))
+
+  expect_equal(example$AdmissionSiteOrgAbbr, 'CV')
+  expect_equal(example$AdmissionSiteOrgName, 'Cardiff & Vale')
+  expect_equal(example$AdmissionSiteOrgFullName, 'Cardiff & Vale University Health Board')
+  expect_equal(length(names(example)), 4)
+
+})
